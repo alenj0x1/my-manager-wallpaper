41dd4796ef5767055b2416f925729a43616f6f1a
alemndev <140274341+alemndev@users.noreply.github.com>
2024-06-08 20:15:24 -0500

    first commit 🐧

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MyWallpaperManager
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button4 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.BackColor = System.Drawing.Color.Bisque;
+      this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.listBox1.Font = new System.Drawing.Font("JetBrains Mono Light", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.ItemHeight = 16;
+      this.listBox1.Location = new System.Drawing.Point(29, 59);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(201, 192);
+      this.listBox1.TabIndex = 0;
+      this.listBox1.Click += new System.EventHandler(this.ListBoxSelect);
+      // 
+      // label1
+      // 
+      this.label1.BackColor = System.Drawing.Color.Transparent;
+      this.label1.Font = new System.Drawing.Font("JetBrains Mono Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.ForeColor = System.Drawing.Color.White;
+      this.label1.Location = new System.Drawing.Point(25, 18);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(340, 29);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "My Wallpaper Manager v1.0.0\r\n";
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+      this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.pictureBox1.Location = new System.Drawing.Point(246, 59);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(377, 392);
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.LightCyan;
+      this.button1.Font = new System.Drawing.Font("JetBrains Mono Light", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.Location = new System.Drawing.Point(29, 272);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(94, 34);
+      this.button1.TabIndex = 3;
+      this.button1.Text = "Add";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.AddButtonClick);
+      // 
+      // button2
+      // 
+      this.button2.BackColor = System.Drawing.Color.Bisque;
+      this.button2.Font = new System.Drawing.Font("JetBrains Mono Light", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2.Location = new System.Drawing.Point(129, 272);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(101, 34);
+      this.button2.TabIndex = 4;
+      this.button2.Text = "Remove";
+      this.button2.UseVisualStyleBackColor = false;
+      this.button2.Click += new System.EventHandler(this.RemoveButtonClick);
+      // 
+      // button3
+      // 
+      this.button3.BackColor = System.Drawing.Color.Lavender;
+      this.button3.Font = new System.Drawing.Font("JetBrains Mono Light", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3.Location = new System.Drawing.Point(29, 312);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(201, 36);
+      this.button3.TabIndex = 5;
+      this.button3.Text = "Set wallpaper";
+      this.button3.UseVisualStyleBackColor = false;
+      this.button3.Click += new System.EventHandler(this.SetWallpaperClick);
+      // 
+      // textBox1
+      // 
+      this.textBox1.BackColor = System.Drawing.Color.Bisque;
+      this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.textBox1.Font = new System.Drawing.Font("JetBrains Mono Light", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textBox1.Location = new System.Drawing.Point(29, 365);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(201, 40);
+      this.textBox1.TabIndex = 6;
+      // 
+      // button4
+      // 
+      this.button4.BackColor = System.Drawing.Color.Honeydew;
+      this.button4.Font = new System.Drawing.Font("JetBrains Mono Light", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button4.Location = new System.Drawing.Point(29, 411);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(201, 40);
+      this.button4.TabIndex = 7;
+      this.button4.Text = "Download";
+      this.button4.UseVisualStyleBackColor = false;
+      this.button4.Click += new System.EventHandler(this.DownloadButtonClick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.White;
+      this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.ClientSize = new System.Drawing.Size(651, 501);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.listBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "Form1";
+      this.Text = "My Wallpaper Manager v1.0.0";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button4;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+
+namespace MyWallpaperManager
+{
+  public partial class Form1 : Form
+  {
+    private readonly List<WallpaperModel> wallpapers = new List<WallpaperModel>();
+
+    public class WallpaperModel
+    {
+      public string ImagePath { get; set; }
+      public bool IsDownloaded { get; set; }
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+    const int SPI_SETDESKWALLPAPER = 20; // Value set wallpaper
+    const int SPIF_UPDATEINIFILE = 0x01; // Save parameters in config user file;
+    const int SPIF_SENDCHANGE = 0x02; // Notify apps change
+
+    private void SetWallpaper(string wallpaper)
+    {
+      SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, wallpaper, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+    }
+
+    public Form1()
+    {
+      InitializeComponent();
+
+      this.FormClosed += ClosedForm;
+
+      // Visualize correctly wallpaper
+      pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+      string[] wallpapersLines = File.ReadAllLines("wallpapers.txt");
+      List<WallpaperModel> listWallpapers = GetWallpapers(wallpapersLines);
+
+      // Save wallpapers in app
+      foreach (WallpaperModel wallpaper in listWallpapers)
+      {
+        wallpapers.Add(wallpaper);
+        listBox1.Items.Add(Path.GetFileName(wallpaper.ImagePath));
+      }
+
+      // Create a folder for storage wallpapers
+      if (!Directory.Exists(@".\images")) Directory.CreateDirectory(@".\images");
+    }
+
+    // Add button
+    private void AddButtonClick(object sender, EventArgs e)
+    {
+      // Select file window
+      OpenFileDialog openFileDialog = new OpenFileDialog()
+      {
+        Filter = " Image files: (*.jpg; *.jpeg; *.png;) | *.jpg; *.jpeg; *.png;"
+      };
+
+      if (openFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        string selectedFile = openFileDialog.FileName;
+
+        WallpaperModel newWallpaper = new WallpaperModel()
+        {
+          ImagePath = selectedFile,
+          IsDownloaded = false
+        };
+
+        wallpapers.Add(newWallpaper);
+        listBox1.Items.Add(Path.GetFileName(newWallpaper.ImagePath));
+      }
+    }
+
+    // Remove button
+    private void RemoveButtonClick(object sender, EventArgs e)
+    {
+      if (listBox1.SelectedIndex < 0) return;
+
+      if (wallpapers.ElementAt(listBox1.SelectedIndex).IsDownloaded)
+      {
+        try
+        {
+          pictureBox1.Image.Dispose(); // ! important, first dispose
+          pictureBox1.Image = null; // after null
+
+          // Delete wallpaper in directory
+          File.Delete(wallpapers.ElementAt(listBox1.SelectedIndex).ImagePath);
+        }
+        catch (Exception)
+        {
+          MessageBox.Show("There was an error trying to download the file.");
+        }
+      }
+
+      // Remove wallpaper view
+      pictureBox1.Image = null;
+
+      wallpapers.RemoveAt(listBox1.SelectedIndex);
+      listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+    }
+
+    // Select wallpaper
+    private void ListBoxSelect(object sender, EventArgs e)
+    {
+      if (listBox1.SelectedIndex < 0) return;
+
+      pictureBox1.Image = new Bitmap(wallpapers.ElementAt(listBox1.SelectedIndex).ImagePath);
+    }
+
+    // Set wallpaper
+    private void SetWallpaperClick(object sender, EventArgs e)
+    {
+      if (listBox1.SelectedIndex < 0) return;
+
+      SetWallpaper(wallpapers[listBox1.SelectedIndex].ImagePath);
+    }
+
+    // Download wallpaper
+    private async void DownloadButtonClick(object sender, EventArgs e)
+    {
+      // Get files count
+      int imagesInPicturesFolder = Directory.GetFiles(@".\images")
+                                                .Where(x => 
+                                                        x.Contains(".jpeg") || 
+                                                        x.Contains(".jpg") || 
+                                                        x.Contains(".png") || 
+                                                        x.Contains(".webp"))
+                                                .Count();
+                                  
+      
+      string pathToDownloadImage = Path.Combine(Environment.CurrentDirectory, "images", $"downloaded_image_{imagesInPicturesFolder + 1}.jpg");
+
+      if (textBox1.Text.Contains(".webp"))
+      {
+        MessageBox.Show("Webp formats are not supported");
+      }
+
+      if (textBox1.Text.Length > 0)
+      {
+        bool downloadImage = await DownloadWallpaperAsync(textBox1.Text, pathToDownloadImage);
+
+        // Check downloaded image
+        if (!downloadImage)
+        {
+          MessageBox.Show("The image could not be downloaded, please check the URL.");
+          return;
+        }
+      }
+      else
+      {
+        MessageBox.Show("Enter a valid URL.");
+        return;
+      }
+
+      WallpaperModel newWallpaper = new WallpaperModel()
+      {
+        ImagePath = pathToDownloadImage,
+        IsDownloaded = true
+      };
+
+      wallpapers.Add(newWallpaper);
+      listBox1.Items.Add(Path.GetFileName(pathToDownloadImage));
+
+      DialogResult alert = MessageBox.Show("Image downloaded successfully.");
+    }
+
+    // Get wallpaper
+    private WallpaperModel GetWallpaper(string wallpaper)
+    {
+      string[] dataWallpaper = wallpaper.Split('|');
+
+      if (dataWallpaper.Length == 2)
+      {
+        WallpaperModel newWallpaper = new WallpaperModel
+        {
+          ImagePath = dataWallpaper[0],
+          IsDownloaded = dataWallpaper[1] == "true"
+        };
+
+        return newWallpaper;
+      }
+
+      return null;
+    }
+
+    // Get wallpapers
+    private List<WallpaperModel> GetWallpapers(string[] wallpapers)
+    {
+      List<WallpaperModel> listWallpapers = new List<WallpaperModel>();
+
+      foreach (string wallpaper in wallpapers)
+      {
+        WallpaperModel newWallpaper = GetWallpaper(wallpaper);
+
+        if (newWallpaper is null) break;
+
+        listWallpapers.Add(newWallpaper);
+      }
+
+      return listWallpapers;
+    }
+
+    // Download wallpaper
+    private async Task<bool> DownloadWallpaperAsync(string url, string savePath)
+    {
+      using (HttpClient client = new HttpClient())
+      {
+        try
+        {
+          // Using http client
+          HttpResponseMessage response = await client.GetAsync(url);
+          response.EnsureSuccessStatusCode();
+
+          byte[] imageBytes = await response.Content.ReadAsByteArrayAsync();
+
+          File.WriteAllBytes(savePath, imageBytes);
+
+          return true;
+        }
+        catch (InvalidOperationException)
+        {
+          return false;
+        }
+        catch (ArgumentException)
+        {
+          return false;
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+      }
+    }
+
+    // Form closed
+    private void ClosedForm(object sender, FormClosedEventArgs e)
+    {
+      using (StreamWriter writer = new StreamWriter("wallpapers.txt"))
+      {
+        foreach (WallpaperModel wallpaper in wallpapers)
+        {
+          StringBuilder line = new StringBuilder();
+
+          line.Append(wallpaper.ImagePath);
+          line.Append("|");
+          line.Append(wallpaper.IsDownloaded.ToString());
+
+          writer.WriteLine(line);
+        }
+      }
+    }
+  }
+}
